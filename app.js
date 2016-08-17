7b732d5b35d07983c44a5a7f89a7eaf8fdc4c681
cdyer <chris.dyer@lowes.com>
2016-08-17 16:33:53 -0400

    Bumping project files to avoid typing.

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-"use strict";
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var app = express();
 
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
-    session = require('express-session'),
-    app = express();
-
+// use sessions for tracking logins
 app.use(session({
-    secret: 'treehouse loves you.',
-    resave: true,
-    saveUninitialized: false
+  secret: 'treehouse loves you',
+  resave: true,
+  saveUninitialized: false
 }));
 
-mongoose.connect('mongodb://localhost:27017/bookworm');
-mongoose.connection.on('error',
-    console.error.bind(console, 'connection error:'));
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/bookworm");
+var db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // serve static files from /public
 app.use(express.static(__dirname + '/public'));
@@ -30,26 +29,27 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
 // include routes
-app.use('/', require('./routes/index'));
+var routes = require('./routes/index');
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('File Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 // define as the last app.use callback
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 // listen on port 3000
-app.listen(3000, function() {
-    console.log('Express app listening on port 3000');
+app.listen(3000, function () {
+  console.log('Express app listening on port 3000');
 });
